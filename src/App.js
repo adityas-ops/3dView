006be457c1d274322680f456c28afe98d04c15da
adityas-opse <85329735+adityas-ops@users.noreply.github.com>
2023-02-17 20:57:07 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Gltf, ScrollControls, useScroll } from "@react-three/drei";
+import { getProject, val } from "@theatre/core";
+import theatreState from "./theatreState.json";
+import {
+  SheetProvider,
+  PerspectiveCamera,
+  useCurrentSheet,
+} from "@theatre/r3f";
+
+export default function App() {
+  const sheet = getProject("Fly Through", { state: theatreState }).sheet(
+    "Scene"
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas gl={{ preserveDrawingBuffer: true }}>
+      <ScrollControls pages={5}>
+        <SheetProvider sheet={sheet}>
+          <Scene />
+        </SheetProvider>
+      </ScrollControls>
+    </Canvas>
   );
 }
+function Scene() {
+  const sheet = useCurrentSheet();
+  const scroll = useScroll();
 
-export default App;
+  // our callback will run on every animation frame
+  useFrame(() => {
+    // the length of our sequence
+    const sequenceLength = val(sheet.sequence.pointer.length);
+    // update the "position" of the playhead in the sequence, as a fraction of its whole length
+    sheet.sequence.position = scroll.offset * sequenceLength;
+  });
+
+  const bgColor = "#84a4f4";
+
+  return (
+    <>
+      <color attach="background" args={[bgColor]} />
+      <fog attach="fog" color={bgColor} near={-4} far={10} />
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[-5, 5, -5]} intensity={1.5} />
+      <Gltf src="/environment.glb" castShadow receiveShadow />
+      <PerspectiveCamera
+        theatreKey="Camera"
+        makeDefault
+        position={[0, 0, 0]}
+        fov={100}
+        near={0.1}
+        far={100}
+      />
+    </>
+  );
+}
